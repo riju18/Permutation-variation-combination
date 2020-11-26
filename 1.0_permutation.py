e7515deef7064e595bf4414b98964576d92981e4
riju18 <rijuchowdhury18@gmail.com>
2020-11-26 23:51:29 +0600

    remove extra code from permuation

--- 1.0_permutation.py
+++ 1.0_permutation.py
@@ -1,10 +1,4 @@
-elements = []
-
 x = int( input( 'Input the number to find out all ways:  ' ) )
-for i in range( x ):
-    element = input( 'enter {} element: '.format( str( i + 1 ) ) )
-    elements.append( element )
-
 f = 1
 count = 0
 temp = ''
